7cdfbd451232cf47736f6964079f65cd2dda96ef
Swaraj Bhumare <swarajbhumare2021@gmail.com>
2026-02-24 22:19:18 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 const kathabook=["swaraj","raj","satyarth","satyarth","satyarth","satyarth","satyarth","satyarth","satyarth","satyarth"];
+console.log('====================================');
+console.log("Swarajya Code ...");
+console.log('====================================');
